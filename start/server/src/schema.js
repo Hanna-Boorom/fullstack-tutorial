d675d7f5f002593b866ffa62be73af9faa5a7288
Hanna Boorom <boorom.hanna@gmail.com>
2021-04-27 15:46:41 -0500

    added typeDefs in schema

---
+++ start/server/src/schema.js
@@ -0,0 +1,42 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }
+
+  type Rocket {
+    id: ID!
+    name: String
+    type: String
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    trips: [Launch]!
+    token: String
+  }
+
+  type Misson {
+    name: String
+    missionPatch(size: PatchSize): String
+  }
+
+  enum PatchSize {
+    SMALL
+    LARGE
+  }
+
+  type Query {
+    launches: [Launch]!
+    launch(id: ID!): Launch
+    me: User
+  }
+`;
+
+module.exports = typeDefs;
